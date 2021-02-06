2ffb74071fdac4d44b8884d0239265a8086bd776
HB9AKM <info@hb9akm.ch>
2021-02-06 23:20:58 +0100

    Implement popup and location change on map

--- src/pages/maps/main.js
+++ src/pages/maps/main.js
@@ -60,7 +60,7 @@ hb9akm.pages.maps =  {
 
 
         var relaisPositions = []
-        hb9akm.pages.maps.relais.forEach(function(el) {
+        hb9akm.pages.maps.relais.forEach(function(el, idx) {
             if (el.status != "qrv") {
                 return;
             }
@@ -69,7 +69,8 @@ hb9akm.pages.maps =  {
                     el.longitude,
                     el.latitude
                 ])),
-                name: el.qthName
+                name: el.qthName,
+                id: idx
             }));
         });
         const layer = new ol.layer.Vector({
@@ -78,6 +79,47 @@ hb9akm.pages.maps =  {
             })
         });
         hb9akm.pages.maps.map.addLayer(layer);
+
+        const popup_el = document.querySelector("div.maps.popup");
+        var popup = new ol.Overlay({
+            element: popup_el,
+            positioning: "bottom-center",
+            stopEvent: false
+        });
+        hb9akm.pages.maps.map.addOverlay(popup);
+        hb9akm.pages.maps.map.on("singleclick", function(ev) {
+            var features = [];
+            hb9akm.pages.maps.map.forEachFeatureAtPixel(
+                ev.pixel,
+                function(feature, layer) {
+                    features.push(feature);
+                    //return feature;
+                }
+            );
+            if (features.length) {
+                document.querySelectorAll(".repeater:not(.template)").forEach(function(el) {
+                    el.parentNode.removeChild(el);
+                });
+                var geometry = features[0].getGeometry();
+                var coord = geometry.getCoordinates();
+                popup.setPosition(coord);
+                popup_el.classList.add("visible");
+                const repeaterTemplate = document.querySelector("div.maps.popup .repeater.template");
+                features.forEach(function(el, index) {
+                    const myTemplate = repeaterTemplate.cloneNode(true);
+                    myTemplate.classList.remove("template");
+                    const repeater = hb9akm.pages.maps.relais[el.values_.id];
+                    myTemplate.querySelector(".name").innerHTML = repeater.qthName;
+                    myTemplate.querySelector(".modes").innerHTML = repeater.modes.map(function(el) { return el.type; }).join(", ");
+                    myTemplate.querySelector(".freq").innerHTML = repeater.qrgTx;
+                    popup_el.appendChild(myTemplate);
+                });
+            } else if (popup_el.classList.contains("visible")) {
+                popup_el.classList.remove("visible");
+            } else {
+                hb9akm.geo.changeToLonLat(ol.proj.toLonLat(ev.coordinate));
+            }
+        });
     },
     addMarker: function(pos, label) {
     },
