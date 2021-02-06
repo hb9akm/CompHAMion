f1a32ab1abe343cafba60bf4ca3958f4fbdefd2f
HB9AKM <info@hb9akm.ch>
2021-02-06 22:46:38 +0100

    Improve Maidenhead parsing

--- src/modules/geo/main.js
+++ src/modules/geo/main.js
@@ -1,3 +1,4 @@
+Math.fmod = function (a,b) { return Number((a - (Math.floor(a / b) * b)).toPrecision(8)); };
 hb9akm.geo = {
     _onload: function() {
     },
@@ -22,31 +23,31 @@ hb9akm.geo = {
     lonLat2Locator: function(lonlat) {
         var lon = lonlat[0] + 180;
         var lat = lonlat[1] + 90;
-        const fieldIndexLon = lon / 20;
-        const fieldIndexLat = lat / 10;
-        lon = lon % 20;
-        lat = lat % 10;
+        const fieldIndexLon = lon / 20 + 65;
+        const fieldIndexLat = lat / 10 + 65;
+        lon = Math.fmod(lon, 20);
+        lat = Math.fmod(lat, 10);
         const squareIndexLon = lon / 2;
         const squareIndexLat = lat / 1;
-        const subSquareIndexLon = lon % 2 / 0.083333;
-        const subSquareIndexLat = lat % 1 / 0.0416665;
-        return String.fromCharCode(65 + fieldIndexLon) +
-            String.fromCharCode(65 + fieldIndexLat) +
+        const subSquareIndexLon = Math.fmod(lon, 2) / 0.083333;
+        const subSquareIndexLat = Math.fmod(lat, 1) / 0.0416665;
+        return String.fromCharCode(fieldIndexLon) +
+            String.fromCharCode(fieldIndexLat) +
             (squareIndexLon.toFixed() - 1) +
             (squareIndexLat.toFixed() - 1) +
-            String.fromCharCode(65 + subSquareIndexLon) +
-            String.fromCharCode(65 + subSquareIndexLat);
+            String.fromCharCode(65 + subSquareIndexLon).toLowerCase() +
+            String.fromCharCode(65 + subSquareIndexLat).toLowerCase();
     },
     locator2lonLat: function(locator) {
-        const fieldIndexLon = locator.charCodeAt(0) - 65;
-        const fieldIndexLat = locator.charCodeAt(1) - 65;
+        locator = locator.toUpperCase();
+        const fieldIndexLon = (locator.charCodeAt(0) - 65).toFixed();
+        const fieldIndexLat = (locator.charCodeAt(1) - 65).toFixed();
         const squareIndexLon = locator[2];
         const squareIndexLat = locator[3];
         const subSquareIndexLon = locator.charCodeAt(4) - 65;
         const subSquareIndexLat = locator.charCodeAt(5) - 65;
         var lon = (subSquareIndexLon / 12) + (1/24);
         lon += squareIndexLon * 2;
-        console.log(lon);
         lon += fieldIndexLon * 20;
         lon -= 180;
         var lat = (subSquareIndexLat / 24) + (1/48);
